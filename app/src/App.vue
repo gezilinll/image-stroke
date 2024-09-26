bfb646aedf59ab876f4206abf4e19f0312119e93
linbinghe <linbinghe@gmail.com>
2024-09-26 21:52:27 +0800

    feat: init project

---
+++ app/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <div><span>Image Stroke</span></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style></style>
---
+++ package/src/index.ts
@@ -0,0 +1,4 @@
+export { FreehandBrush } from './brush';
+export { Point } from './common/point';
+export { Rect } from './common/rect';
+export * from './options';
