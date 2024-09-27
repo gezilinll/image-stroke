053f53a4fa734b3e706722e715ad2d3ee45e3b53
linbinghe <linbinghe@gmail.com>
2024-09-27 17:32:27 +0800

    feat: setup ui

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,50 @@
 <template>
     <div>
-        <input type="file" @change="onFileChange" accept=".png, .jpg, .jpeg" />
-        <img ref="srcImage" src="" alt="图片预览" />
-        <canvas ref="result"></canvas>
+        <div class="container">
+            <div style="display: flex">
+                <div class="config-area">
+                    <input
+                        type="file"
+                        id="selectedFile"
+                        style="display: none"
+                        @change="onFileChange"
+                    />
+                    <input
+                        type="button"
+                        value="Select Image"
+                        @click="selectFile"
+                        style="padding: 6px"
+                    />
+                    <div
+                        style="width: 90%; height: 1px; background-color: black; margin-top: 10px"
+                    ></div>
+                    <div class="config-item">
+                        <label>Width: </label>
+                        <input v-model.number="lineWidth" type="range" min="0" max="100" />
+                        <span style="margin-left: 5px">{{ lineWidth }}</span>
+                    </div>
+
+                    <div class="config-item">
+                        <label>Style: </label>
+                        <label>
+                            <input type="radio" value="solid" v-model="styleType" /> Solid
+                        </label>
+                        <label>
+                            <input type="radio" value="dashed" v-model="styleType" /> Dashed
+                        </label>
+                    </div>
+                    <div class="config-item">
+                        <label>Debug: </label>
+                        <input
+                            type="number"
+                            v-model.number="debugDistance"
+                            style="margin-left: 5px; width: 120px"
+                        />
+                    </div>
+                </div>
+                <canvas ref="result" class="canvas-area"></canvas>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -10,23 +52,30 @@
 import { SDF } from 'image-stroke';
 import { Ref, ref } from 'vue';
 
-const srcImage: Ref<HTMLImageElement | null> = ref(null);
 const result: Ref<HTMLCanvasElement | null> = ref(null);
+const debugDistance = ref(-999);
+const lineWidth = ref(0);
+const styleType = ref('solid');
+
+function selectFile() {
+    document.getElementById('selectedFile')!.click();
+}
 
 function onFileChange(event: any) {
     const file = event.target.files[0];
     if (file) {
         const reader = new FileReader();
         reader.onload = () => {
-            srcImage.value!.src = reader.result as any;
-            srcImage.value!.onload = () => {
+            const imageLoader = new Image();
+            imageLoader.src = reader.result as any;
+            imageLoader.onload = () => {
                 console.time('sdf');
-                const sdf = new SDF(srcImage.value!);
+                const sdf = new SDF(imageLoader);
                 const data = sdf.recognize();
                 console.timeEnd('sdf');
                 const canvas = result.value!;
-                canvas.width = srcImage.value!.width;
-                canvas.height = srcImage.value!.height;
+                canvas.width = imageLoader.width;
+                canvas.height = imageLoader.height;
                 const ctx = canvas.getContext('2d')!;
                 const imageData = ctx.createImageData(canvas.width, canvas.height);
                 for (let i = 0; i < data.length; i++) {
@@ -44,4 +93,40 @@ function onFileChange(event: any) {
 }
 </script>
 
-<style></style>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+}
+.container {
+    position: absolute;
+    background-color: rgb(233, 235, 237);
+    width: 100%;
+    height: 100%;
+}
+.config-area {
+    margin: 16px;
+    width: 200px;
+    height: 200px;
+    padding: 16px;
+    box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.3);
+    background-color: white;
+}
+.canvas-area {
+    border: 1px solid #ccc;
+    margin-top: 16px;
+    box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.3);
+}
+
+.config-item {
+    margin-top: 6px;
+    display: flex;
+    align-items: center;
+    padding-top: 6px;
+    padding-bottom: 6px;
+}
+</style>
